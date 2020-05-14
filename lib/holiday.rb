111fc8924d3654a436f5cde45652225e2d89e623
Peter Ort <peter@peterort.com>
2020-05-14 20:28:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,5 +81,13 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  array = []
+  holiday_hash.each do |season, holiday, item|
+    if item == "BBQ"
+      array << item
+    end
+    array
+      
+    
 
 end
